2a63b92d7f1f81086eedf39bf7a4dba21afbf9b1
Aleksey Sokolov <sokolov.alexey@me.com>
2020-11-29 23:51:26 +0300

    initial commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,46 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
+
+//Task 1
+
+String.prototype.isPalindrome = function () {
+  const string = this.toLocaleLowerCase().replace(/\s+/g, '');
+  const reverseString = this.toLocaleLowerCase()
+    .split('')
+    .reverse()
+    .join('')
+    .replace(/\s+/g, '');
+
+  return string === reverseString;
+};
+
+console.log('А роза упала на лапу Азора'.isPalindrome());
+
+//Task 2
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  if (marks.length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i];
+  }
+
+  const average = sum / marks.length;
+  const roundedAverage = Math.round(average);
+
+  return roundedAverage;
 }
 
+//Task 3
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  const now = Date.now();
+  birthday = +new Date(birthday).getTime();
+  const diff = now - birthday;
+  const age = diff / (1000 * 60 * 60 * 24 * 365.25);
+
+  return age >= 18;
 }
